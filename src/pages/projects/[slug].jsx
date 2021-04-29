48242ffab410c8253808e389de7a5b02523e7374
Mathias Riis Sørensen <72079659+mathiassdev@users.noreply.github.com>
2021-04-29 08:02:10 +0200

    Updating Dynamic Pages
    Updating other dynamic pages to leverage Rich Text content.

--- src/pages/projects/[slug].jsx
+++ src/pages/projects/[slug].jsx
@@ -1,9 +1,14 @@
 import Head from "next/head";
 import HeaderBackground from "../../components/backgrounds/HeaderBackground";
+import Image from "next/image";
+import dynamic from "next/dynamic";
 import styled from "styled-components";
+import RichTextPageContentStyles from "../../components/RichTextPageContent/Styles/RichTextPageContent.module.css";
+import TypographyStyles from "../../components/RichTextPageContent/Styles/Typography.module.css";
+import { formatDate } from "../../../utils/functions";
 import { getProjects, getProject } from "../../../utils/contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS } from "@contentful/rich-text-types";
+import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types";
 import { CameraIcon } from "@heroicons/react/solid";
 
 export async function getStaticPaths() {
@@ -25,11 +30,174 @@ export async function getStaticProps(context) {
   };
 }
 
+function slugifyString(string) {
+  return string
+    .replace(/\s+/g, "-")
+    .replace(/[^\w\-]+/g, "")
+    .replace(/\-\-+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "")
+    .toLowerCase();
+}
+
+const DynamicCodeBlock = dynamic(() =>
+  import("../../components/RichTextPageContent/CodeBlock/")
+);
+
+const DynamicVideoEmbed = dynamic(() =>
+  import("../../components/RichTextPageContent/VideoEmbed")
+);
+
+export function getRichTextRenderOptions(links, options) {
+  const { renderH2Links, renderNativeImg } = options;
+
+  const assetBlockMap = new Map(
+    links?.assets?.block?.map((asset) => [asset.sys.id, asset])
+  );
+
+  const entryBlockMap = new Map(
+    links?.entries?.block?.map((entry) => [entry.sys.id, entry])
+  );
+
+  return {
+    renderMark: {
+      [MARKS.BOLD]: (text) => (
+        <b
+          className={`${TypographyStyles.bodyCopy} ${TypographyStyles.bodyCopy__bold}`}
+        >
+          {text}
+        </b>
+      ),
+      [MARKS.CODE]: (text) => (
+        <code className={TypographyStyles.inlineCode}>{text}</code>
+      ),
+    },
+
+    renderNode: {
+      [INLINES.HYPERLINK]: (node, children) => (
+        <a
+          className={TypographyStyles.inlineLink}
+          href={node.data.uri}
+          target="_blank"
+          rel="nofollow noreferrer"
+        >
+          {children}
+        </a>
+      ),
+      [BLOCKS.HR]: (text) => (
+        <hr className={RichTextPageContentStyles.page__hr} />
+      ),
+      [BLOCKS.HEADING_1]: (node, children) => (
+        <h1 className={TypographyStyles.heading__h1}>{children}</h1>
+      ),
+      [BLOCKS.HEADING_2]: (node, children) => {
+        if (renderH2Links) {
+          return (
+            <div
+              className={RichTextPageContentStyles.page__linkedHeaderContainer}
+            >
+              <h2
+                id={`${slugifyString(children[0])}`}
+                className={TypographyStyles.heading__h2}
+              >
+                {children}
+              </h2>
+              <a
+                className={`${RichTextPageContentStyles.page__headerLink} ${TypographyStyles.inlineLink}`}
+                href={`#${slugifyString(children[0])}`}
+                aria-label={children}
+              >
+                <LinkIcon />
+              </a>
+            </div>
+          );
+        } else {
+          <h2 className={TypographyStyles.heading__h2}>{children}</h2>;
+        }
+      },
+      [BLOCKS.HEADING_3]: (node, children) => (
+        <h3 className={TypographyStyles.heading__h3}>{children}</h3>
+      ),
+      [BLOCKS.HEADING_4]: (node, children) => (
+        <h4 className={TypographyStyles.heading__h4}>{children}</h4>
+      ),
+      [BLOCKS.HEADING_5]: (node, children) => (
+        <h5 className={TypographyStyles.heading__h5}>{children}</h5>
+      ),
+      [BLOCKS.HEADING_6]: (node, children) => (
+        <h6 className={TypographyStyles.heading__h6}>{children}</h6>
+      ),
+      [BLOCKS.PARAGRAPH]: (node, children) => (
+        <p className={TypographyStyles.bodyCopy}>{children}</p>
+      ),
+      [BLOCKS.QUOTE]: (node, children) => (
+        <blockquote className={TypographyStyles.blockquote}>
+          {children}
+        </blockquote>
+      ),
+      [BLOCKS.UL_LIST]: (node, children) => (
+        <ul className={RichTextPageContentStyles.page__ul}>{children}</ul>
+      ),
+      [BLOCKS.OL_LIST]: (node, children) => (
+        <ol className={RichTextPageContentStyles.page__ol}>{children}</ol>
+      ),
+      [BLOCKS.LIST_ITEM]: (node, children) => (
+        <li
+          className={`${TypographyStyles.bodyCopy} ${RichTextPageContentStyles.page__li}`}
+        >
+          {children}
+        </li>
+      ),
+      [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+        const entry = entryBlockMap.get(node.data.target.sys.id);
+        const { __typename } = entry;
+
+        switch (__typename) {
+          case "VideoEmbed":
+            const { embedUrl, title } = entry;
+            return <DynamicVideoEmbed embedUrl={embedUrl} title={title} />;
+          case "CodeBlock":
+            const { language, code } = entry;
+
+            return <DynamicCodeBlock language={language} code={code} />;
+          default:
+            return null;
+        }
+      },
+      [BLOCKS.EMBEDDED_ASSET]: (node, next) => {
+        const { title, url, height, width, description } = assetBlockMap.get(
+          node.data.target.sys.id
+        );
+        if (renderNativeImg) {
+          return (
+            <div className={RichTextPageContentStyles.page__imgContainer}>
+              <img src={url} alt={description} height={height} width={width} />
+            </div>
+          );
+        } else {
+          return (
+            <div className={RichTextPageContentStyles.page__imgContainer}>
+              <Image
+                src={url}
+                alt={description}
+                height={height}
+                width={width}
+                layout="responsive"
+              />
+            </div>
+          );
+        }
+      },
+    },
+  };
+}
+
 const Wrapper = styled.div``;
 
 const Author = styled.div``;
 
-export default function ProjectPost({ project }) {
+export default function ProjectPost({ project, renderH2Links }) {
+  project = formatDate(project);
   return (
     <Wrapper className="mt-40">
       <Head>
@@ -43,8 +211,10 @@ export default function ProjectPost({ project }) {
           <div className="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
             <div>
               <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-                Case Study
+                Case Study - {""}
+                {project.date}
               </h2>
+
               <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl">
                 {project.title}
               </h3>
@@ -132,7 +302,13 @@ export default function ProjectPost({ project }) {
             </div>
             <div className="mt-8 lg:mt-0">
               <div className="mt-6 prose md:prose-xl dark:prose-dark dark:md:prose-xl-dark mx-auto lg:row-start-1 lg:col-start-1">
-                {documentToReactComponents(project.content.json)}
+                {documentToReactComponents(
+                  project.content.json,
+                  getRichTextRenderOptions(
+                    project.content.links,
+                    (renderH2Links = "true")
+                  )
+                )}
               </div>
             </div>
           </div>
