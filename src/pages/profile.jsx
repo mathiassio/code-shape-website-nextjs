1ce8c1220e4bed5530f2153a73f9b6044ec9a8c7
Mathias Riis Sørensen <72079659+mathiassdev@users.noreply.github.com>
2021-05-30 20:33:54 +0200

    Update profile.jsx

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -16,7 +16,7 @@ export default function ProfilePage() {
   const auth = useAuth();
 
   return auth.user ? (
-    <Wrapper>
+    <Wrapper className="mx-5">
       <PageIntro
         title="Welcome"
         description="While we wait for more content to arrive, go ahead and explore the articles and the community."
@@ -24,41 +24,43 @@ export default function ProfilePage() {
       />
 
       <section aria-labelledby="profile-overview-title">
-        <div className="rounded-lg overflow-hidden shadow ">
+        <div className="rounded-lg overflow-hidden shadow max-w-3xl m-auto bg-gray-50 dark:bg-gray-darkest">
           <h2 className="sr-only" id="profile-overview-title">
             Profile Overview
           </h2>
-          <div className="p-6 max-w-2xl mx-auto">
-            <div className="sm:flex sm:items-center sm:justify-between">
-              <div className="sm:flex sm:space-x-5">
-                <div className="flex-shrink-0">
-                  <img
-                    className="mx-auto h-20 w-20 rounded-full"
-                    src={auth.user.photoUrl}
-                    alt=""
-                  />
-                </div>
-                <div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
-                  <p className="text-xl font-bold gradient sm:text-2xl">
-                    {auth.user.name}
-                  </p>
-                  <p className="text-sm font-medium text-gray-600">
-                    {auth.user.email},{" "}
-                    <p>
-                      Signed in with{" "}
-                      <span className="gradient">{auth.user.provider}</span>
+          <div>
+            <div className="p-6 max-w-2xl mx-auto">
+              <div className="sm:flex sm:items-center sm:justify-between">
+                <div className="sm:flex sm:space-x-5">
+                  <div className="flex-shrink-0">
+                    <img
+                      className="mx-auto h-20 w-20 rounded-full"
+                      src={auth.user.photoUrl}
+                      alt=""
+                    />
+                  </div>
+                  <div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
+                    <p className="text-xl font-bold gradient sm:text-2xl">
+                      {auth.user.name}
                     </p>
-                  </p>
-                  <p></p>
+                    <p className="text-sm font-medium text-gray-600">
+                      {auth.user.email},{" "}
+                      <p>
+                        Signed in with{" "}
+                        <span className="gradient">{auth.user.provider}</span>
+                      </p>
+                    </p>
+                    <p></p>
+                  </div>
                 </div>
-              </div>
-              <div className="mt-5 flex justify-center sm:mt-0">
-                <a
-                  href="/articles"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md md:py-4 md:text-lg md:px-10 text-gray-500 transition-colors duration-150 border-gray-500 focus:shadow-outline hover:border-black hover:text-black dark:hover:text-white dark:hover:border-white"
-                >
-                  Explore Content{" "}
-                </a>
+                <div className="mt-5 flex justify-center sm:mt-0">
+                  <a
+                    href="/articles"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md md:py-4 md:text-lg md:px-10 text-gray-500 transition-colors duration-150 border-gray-500 focus:shadow-outline hover:border-black hover:text-black dark:hover:text-white dark:hover:border-white"
+                  >
+                    Explore Content{" "}
+                  </a>
+                </div>
               </div>
             </div>
           </div>
@@ -66,6 +68,6 @@ export default function ProfilePage() {
       </section>
     </Wrapper>
   ) : (
-    <div>Loading...</div>
+    <div className="text-center">Loading...</div>
   );
 }
