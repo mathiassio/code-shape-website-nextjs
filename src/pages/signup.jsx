67ea289ca9af952a6fae1cbc0dd3b166ea49bce1
Mathias Riis Sørensen <72079659+mathiassdev@users.noreply.github.com>
2021-06-11 07:59:56 +0200

    Update signup.jsx
    Adding full name

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -21,6 +21,25 @@ const SignIn = () => {
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label
+                htmlFor="name"
+                className="block text-sm font-medium text-logoBlue-dark"
+              >
+                Full Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="name"
+                  name="name"
+                  type="name"
+                  autoComplete="name"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
                 htmlFor="email"
                 className="block text-sm font-medium text-logoBlue-dark"
               >
