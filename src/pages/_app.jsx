d24ff2aa3cb6d8e7e41a6f65c6946040c1cb4ab7
Mathias Riis Sørensen <72079659+mathiassdev@users.noreply.github.com>
2021-09-20 21:02:21 +0200

    WEB-79: Google Analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,23 +4,39 @@ import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 import { AuthProvider } from "../lib/auth";
 import { motion } from "framer-motion";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps, router }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <AuthProvider>
-    <motion.div
-      key={router.route}
-      initial="pageInitial"
-      animate="pageAnimate"
-      variants={{
-        pageInitial: {
-          opacity: 0,
-        },
-        pageAnimate: {
-          opacity: 1,
-        },
-      }}
-    >
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
         <GlobalStyle />
         <Header />
         <Component {...pageProps} />
